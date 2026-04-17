b93336978f1f8081a8dddb015919df372f3b0098
Saruulbilegt <saruul15987@gmail.com>
2026-04-17 18:19:24 +0800

    fix menu system.sh

--- month01/week03/Friday/Scripts/menu_sytem.sh
+++ month01/week03/Friday/Scripts/menu_sytem.sh
@@ -2,17 +2,14 @@
 
 read -p "Please enter any number(1-10), enter 0 to quit: " N
 
-if [ $N -eq 0 ]; then
-        exit 0
-fi
-
-while [ $N -le 10 ]; do 
+while [ $N -ne 0 ]; do 
 	for ((i=1; i<=10; i++)); do
 		SUM=$((i * N))
 		echo "$N * $i = $SUM"
 	done
 	read -p "Please enter any number(1-10), enter 0 to quit: " N
-	if [ $N -eq 0 ]; then
-        exit 0
-	fi
 done
+
+if [ $N -eq 0 ]; then
+	exit 0
+fi
