ddf9d2e86adace044829e880d51a4bf6073b712d
Saruulbilegt <saruul15987@gmail.com>
2026-04-30 17:58:16 +0800

    forgot

---
+++ month02/week01/day04/Whileloop.java
@@ -0,0 +1,17 @@
+package month02.week01.day04;
+
+public class Whileloop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i + " ");
+            i++;
+        }
+        i = 2;
+        while (i < 9) {
+            System.out.println(i + "");
+            i = i + 2;
+        }
+        System.out.println();
+    }
+}
---
+++ month02/week01/day04/forloop.java
@@ -0,0 +1,13 @@
+package month02.week01.day04;
+
+public class forloop {
+    public static void main(String[] args) {
+        for (int i = 3; i <= 5; i++) {
+            System.out.println(i + "");
+        }
+
+        for (int i = 2; i <= 8; i = i + 2) {
+            System.out.println(i + " ");
+        }
+    }
+}
